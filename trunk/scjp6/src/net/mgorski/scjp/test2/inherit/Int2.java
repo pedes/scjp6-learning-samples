dc6c3503498afb30b5dcd4f718286f184b48a238
ski.rulez@gmail.com <ski.rulez@gmail.com@fc8b07a9-e9ee-75e7-2ecf-1ce64d642ec6>
2012-03-27 13:57:15 +0000

    various tests

---
+++ trunk/scjp6/src/net/mgorski/scjp/test2/inherit/Int2.java
@@ -0,0 +1,7 @@
+package net.mgorski.scjp.test2.inherit;
+
+public interface Int2 {
+    
+    void go();
+
+}
---
+++ trunk/scjp6/src/net/mgorski/scjp/test2/inherit/Int3.java
@@ -0,0 +1,5 @@
+package net.mgorski.scjp.test2.inherit;
+
+public interface Int3 extends Int1, Int2{
+
+}
