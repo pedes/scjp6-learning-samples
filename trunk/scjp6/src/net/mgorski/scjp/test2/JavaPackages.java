dd0cba6a95767f4069b200ef8082f227b5098cf3
ski.rulez@gmail.com <ski.rulez@gmail.com@fc8b07a9-e9ee-75e7-2ecf-1ce64d642ec6>
2012-04-16 15:16:32 +0000

    pattern/matcher

--- trunk/scjp6/src/net/mgorski/scjp/test2/JavaPackages.java
+++ trunk/scjp6/src/net/mgorski/scjp/test2/JavaPackages.java
@@ -1,19 +1,27 @@
 package net.mgorski.scjp.test2;
 
-import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavaPackages {
-    
+
     public static void main(String[] args) {
-        java.util.regex.Pattern p;
-        java.util.regex.Matcher m;
+
+        // digits (all digits)
+        patternTester("(\\d)+", "wht the 666 heck 667 sdfsdfs 0\\  # 123123  23 23 55L 0000 0.3 \n ");
         
-        StringBuilder sb;
-        
-        Math.abs(3l);
-        
-        Integer i = Integer.parseInt("1");
-        i.intValue();
+        // all hexes (each separately)
+        patternTester("[0-9a-f]", "wht the 666 heck 667 sdfsdfs 0\\  # 123123  23 23 55L 0000 0.3 \n ");
+    }
+    
+    private static void patternTester(String pattern, String matcher){
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(matcher);
+        System.out.println("---------");
+        System.out.println("pattern: " +pattern +"\nmatcher: " +matcher);
+        while (m.find()) {
+            System.out.println("Group : [" + m.start() + "," + m.end() + ")='" + m.group()+"'");
+        }
     }
 
 }
